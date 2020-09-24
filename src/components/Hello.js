18cc8ab62d678384ecd3a0762924e8330c054fd0
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2020-09-24 15:03:45 -0400

    initial

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1> Hello! {this.props.foo} </h1>
+    );
+  }
+
+}
+
+export default Hello;
